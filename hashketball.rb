4cdf982c0de9839bac416c4cafc6b8098b239c70
Max Miller <maxmiller413@gmail.com>
2021-01-19 00:21:06 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,17 @@
 # Write your code here!
+require 'pry'
+
+def game_hash 
+  hash = {
+    :home => 
+      {:team_name => {}, 
+      :colors => {}, 
+      :players => [alan_anderson, reggie_evans, brook_lopez, mason_plumlee, jason_terry]
+    }, 
+    :away => 
+      {:team_name => {}, 
+      :colors => {}, 
+      :players => [jeff_adrien, bismack_biyombo, desagna_diop, ben_gordon, kemba_walker]
+    }
+  }
+end 
