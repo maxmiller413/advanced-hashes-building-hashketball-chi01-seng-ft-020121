f7e8bdc2d276a7ca887085f82da4126c1174865d
Max Miller <maxmiller413@gmail.com>
2021-01-31 21:00:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,14 +3,23 @@ require 'pry'
 
 def game_hash 
   {:home=>
-    {:team_name=>"Brooklyn Nets", :colors=>["Black", "White"],
+    {:team_name=>"Brooklyn Nets", 
+    :colors=>["Black", "White"],
     :players=>[
-      {:player_name =>alan_anderson, :player_name =>reggie_evans, :player_name =>brook_lopez,:player_name =>mason_plumlee, :player_name =>jason_terry},
-      }]}, 
+      {:player_name =>alan_anderson}, 
+      {:player_name =>reggie_evans}, 
+      {:player_name =>brook_lopez},
+      {:player_name =>mason_plumlee}, 
+      {:player_name =>jason_terry},
+      ]}, 
   :away=>
     {:team_name=> "Charlotte Hornets", :colors=>["Turquoise", "Purple"], 
     :players=>[
-      {:player_name =>jeff_adrien, :player_name =>bismack_biyombo, :player_name =>desagna_diop, :player_name =>ben_gordon, :player_name =>kemba_walker,
-      }]}
+      {:player_name =>jeff_adrien}, 
+      {:player_name =>bismack_biyombo}, 
+      {:player_name =>desagna_diop}, 
+      {:player_name =>ben_gordon}, 
+      {:player_name =>kemba_walker},
+      ]}
   }
 end 
