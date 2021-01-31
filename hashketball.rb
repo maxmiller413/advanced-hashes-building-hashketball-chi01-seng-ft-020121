66c887aa1211cb269d668495c9e334bc34d6301f
Max Miller <maxmiller413@gmail.com>
2021-01-31 21:06:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,20 +6,111 @@ def game_hash
     {:team_name=>"Brooklyn Nets", 
     :colors=>["Black", "White"],
     :players=>[
-      {:player_name =>alan_anderson}, 
-      {:player_name =>reggie_evans}, 
-      {:player_name =>brook_lopez},
-      {:player_name =>mason_plumlee}, 
-      {:player_name =>jason_terry},
+      {:player_name =>alan_anderson,
+      :number=>0,
+      :shoe=>16,
+      :points=>22,
+      :rebounds=>12,
+      :assists=>12,
+      :steals=>3,
+      :blocks=>1,
+      :slam_dunks=>1
+      }, 
+      {:player_name =>reggie_evans,
+      :number=>30,
+      :shoe=>14,
+      :points=>12,
+      :rebounds=>12,
+      :assists=>12,
+      :steals=>12,
+      :blocks=>12,
+      :slam_dunks=>7
+      }, 
+      {:player_name =>brook_lopez,
+      :number=>11,
+      :shoe=>17,
+      :points=>17,
+      :rebounds=>19,
+      :assists=>10,
+      :steals=>3,
+      :blocks=>1,
+      :slam_dunks=>15
+      },
+      {:player_name =>mason_plumlee
+      :number=>
+      :shoe=>
+      :points=>
+      :rebounds=>
+      :assists=>
+      :steals=>
+      :blocks=>
+      :slam_dunks=>
+      }, 
+      {:player_name =>jason_terry
+      :number=>
+      :shoe=>
+      :points=>
+      :rebounds=>
+      :assists=>
+      :steals=>
+      :blocks=>
+      :slam_dunks=>
+      },
       ]}, 
   :away=>
-    {:team_name=> "Charlotte Hornets", :colors=>["Turquoise", "Purple"], 
+    {:team_name=> "Charlotte Hornets", 
+    :colors=>["Turquoise", "Purple"], 
     :players=>[
-      {:player_name =>jeff_adrien}, 
-      {:player_name =>bismack_biyombo}, 
-      {:player_name =>desagna_diop}, 
-      {:player_name =>ben_gordon}, 
-      {:player_name =>kemba_walker},
+      {:player_name =>jeff_adrien,
+      :number=>
+      :shoe=>
+      :points=>
+      :rebounds=>
+      :assists=>
+      :steals=>
+      :blocks=>
+      :slam_dunks=>
+      }, 
+      {:player_name =>bismack_biyombo,
+      :number=>
+      :shoe=>
+      :points=>
+      :rebounds=>
+      :assists=>
+      :steals=>
+      :blocks=>
+      :slam_dunks=>
+      }, 
+      {:player_name =>desagna_diop,
+      :number=>
+      :shoe=>
+      :points=>
+      :rebounds=>
+      :assists=>
+      :steals=>
+      :blocks=>
+      :slam_dunks=>
+      }, 
+      {:player_name =>ben_gordon,
+      :number=>
+      :shoe=>
+      :points=>
+      :rebounds=>
+      :assists=>
+      :steals=>
+      :blocks=>
+      :slam_dunks=>
+      }, 
+      {:player_name =>kemba_walker,
+      :number=>
+      :shoe=>
+      :points=>
+      :rebounds=>
+      :assists=>
+      :steals=>
+      :blocks=>
+      :slam_dunks=>
+      },
       ]}
   }
 end 
